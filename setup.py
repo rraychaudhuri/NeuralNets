10310c604d649e236a3cd92240df5689a5833495
Rajat Raychaudhuri <rrc.999@gmail.com>
2018-11-24 07:20:22 -0800

    Minor Update

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='NeuralNet',
+    version='1.0.0',
+    description="Numpy based rusty implementation while reading Michael Nielsen's Deep Learning Book",
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Rajat Raychaudhuri',
+    author_email='rrc.999@gmail.com',
+    url='https://github.com/rraychaudhuri/NeuralNets',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
